37f00ba4087a27a630cdb1a1e7514f54589620ac
Sriditya Raj <rajsriditya@gmail.com>
2020-12-26 00:02:56 +0530

    Added Merge Sorted Arrays

---
+++ DataStructuresAndAlgorithms/MergeSortedArrays.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAndAlgorithms
+{
+    public class MergeSortedArrays
+    {
+        public MergeSortedArrays()
+        {
+
+        }
+
+        public int[] MergeTwoSortedArrays(int[] array1 , int[] array2)
+        {
+            int len1 = array1.Length;
+            int len2 = array2.Length;
+            int[] mergedArray = new int[len1 + len2];
+
+            int i = 0, j = 0, k = 0;
+            while(i < len1 && j < len2)
+            {
+                if(array1[i] > array2[j])
+                {
+                    mergedArray[k++] = array2[j++];
+
+                }
+
+                else if(array1[i] < array2[j])
+                {
+                    mergedArray[k++] = array1[i++];
+                }
+
+                else
+                {
+                    mergedArray[k++] = array1[i++];
+                    mergedArray[k++] = array2[j++];
+                }
+            }
+
+            if(i < len1)
+            {
+                while(i < len1)
+                {
+                    mergedArray[k++] = array1[i++];
+                }
+            }
+
+            if( j < len2)
+            {
+                while(j < len2)
+                {
+                    mergedArray[k++] = array2[j++];
+                }
+            }
+            return mergedArray;
+        }
+    }
+}
--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -6,9 +6,14 @@ namespace DataStructuresAndAlgorithms
     {
         static void Main(string[] args)
         {
-            var array = new StringReversal();
-            var reversedString = array.ReverseString2("Hi My name is Sriditya");
-            Console.WriteLine(reversedString);
+            //var array = new StringReversal();
+            //var reversedString = array.ReverseString2("Hi My name is Sriditya");
+            //Console.WriteLine(reversedString);
+
+            var a = new MergeSortedArrays();
+            int[] array1 = { 1, 2, 6, 78, 99 };
+            int[] array2 = { 5, 6, 8, 66, 90, 110 };
+            var mergesSortedArrays = a.MergeTwoSortedArrays(array1, array2);
         }
     }
 }
