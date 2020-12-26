2c48f5044a02eea2d42bf0c188a43024f8f9c077
Sriditya Raj <rajsriditya@gmail.com>
2020-12-27 01:57:17 +0530

    Linked List Implementation

--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -32,10 +32,26 @@ namespace DataStructuresAndAlgorithms
 
             //var keys = hashClass.GetKeys();
 
-            var x = new FindFirstRecurringCharacter();
-            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4 };
-            var firstRecurringCharacter = x.FindFirstRecurringNumberUsingHash(arr);
-            var firstRecurringCharacterUsingList = x.FindFirstRecurringNumberUsingList(arr);
+            //var x = new FindFirstRecurringCharacter();
+            //int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4 };
+            //var firstRecurringCharacter = x.FindFirstRecurringNumberUsingHash(arr);
+            //var firstRecurringCharacterUsingList = x.FindFirstRecurringNumberUsingList(arr);
+
+            var singlyLinkedList = new SinglyLinkedList();
+            singlyLinkedList.Append(1);
+            singlyLinkedList.Append(5);
+            singlyLinkedList.Append(10);
+            singlyLinkedList.Append(1);
+            singlyLinkedList.Prepend(15);
+            singlyLinkedList.Append(20);
+            singlyLinkedList.InsertAtindex(3, 110);
+            singlyLinkedList.InsertAtindex(0, 32);
+            singlyLinkedList.InsertAtindex(7, 150);
+            singlyLinkedList.DeleteAtIndex(4);
+            singlyLinkedList.ReverseLinkedList();
+            singlyLinkedList.PrintList();
+
+
 
 
         }
---
+++ DataStructuresAndAlgorithms/SinglyLinkedList.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAndAlgorithms
+{
+    public class LinkedListNode
+    {
+        public int value { get; set; }
+        public LinkedListNode next { get; set; }
+
+        public LinkedListNode(int _value)
+        {
+            value = _value;
+            next = null;
+        }
+    }
+    public class SinglyLinkedList
+    {
+        private LinkedListNode head;
+        private LinkedListNode tail;
+        private int length;
+        public SinglyLinkedList()
+        {
+            length = 0;
+        }
+
+        public void Append(int value)
+        {
+            if(length == 0)
+            {
+                CreateFirstNode(value);
+                return;
+            }
+
+            var newTail = new LinkedListNode(value);
+            newTail.next = null;
+            tail.next = newTail;
+            tail = newTail;
+            length++;
+        }
+
+        public void Prepend(int value)
+        {
+            if(length == 0)
+            {
+                CreateFirstNode(value);
+                return;
+            }
+
+            var newHead = new LinkedListNode(value);
+            newHead.next = head;
+            head = newHead;
+            length++;
+        }
+
+        public void InsertAtindex(int index, int value)
+        {
+            if(index == 0)
+            {
+                Prepend(value);
+                return;
+            }
+
+            if(index == length-1)
+            {
+                Append(value);
+                return;
+            }
+
+            LinkedListNode prevNode = head;
+
+            for(int i = 0; i < index - 1; i++)
+            {
+                prevNode = prevNode.next;
+            }
+
+            var newNode = new LinkedListNode(value);
+            newNode.next = prevNode.next;
+            prevNode.next = newNode;
+            length++;
+        }
+
+        public void DeleteAtIndex(int index)
+        {
+            if(index == 0)
+            {
+                head = head.next;
+                length--;
+                return;
+            }
+
+            LinkedListNode prevNode = head;
+            for(int i = 0; i < index - 1; i++)
+            {
+                prevNode = prevNode.next;
+            }
+
+            LinkedListNode nodeToDelete = prevNode.next;
+            prevNode.next = nodeToDelete.next;
+            length--;
+        }
+
+        public void ReverseLinkedList()
+        {
+            if(length == 1)
+            {
+                return;
+            }
+
+            LinkedListNode prevNode = head;
+            head = tail;
+            LinkedListNode nextNode = prevNode.next;
+
+            for(int i = 0; i < length - 1; i++)
+            {
+                LinkedListNode temp = nextNode.next;
+                nextNode.next = prevNode;
+                prevNode = nextNode;
+                nextNode = temp;
+            }
+
+            tail = nextNode;
+        }
+
+        public void PrintList()
+        {
+            int count = length;
+            if(count == 0)
+            {
+                Console.Write("No data in linked list");
+                return;
+            }
+
+            while(count != 0)
+            {
+                Console.Write("-->" + head.value.ToString());
+                head = head.next;
+                count--;
+            }
+        }
+
+        public void CreateFirstNode(int value)
+        {
+            head = new LinkedListNode(value);
+            tail = head;
+            length++;
+        }
+    }
+}
