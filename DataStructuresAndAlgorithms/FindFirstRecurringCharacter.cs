006b9f7fe4e094d8dcf84ce69fd6f3a7f230f929
Sriditya Raj <rajsriditya@gmail.com>
2020-12-26 23:17:20 +0530

    First Recurring character in an array

---
+++ DataStructuresAndAlgorithms/FindFirstRecurringCharacter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAndAlgorithms
+{
+    public class FindFirstRecurringCharacter
+    {
+        public FindFirstRecurringCharacter()
+        {
+
+        }
+
+        public int FindFirstRecurringNumberUsingHash(int[] items)
+        {
+            var temp = new Hashtable();
+
+            for(int i = 0; i < items.Length; i++)
+            {
+                if(temp.ContainsKey(items[i]))
+                {
+                    return items[i];
+                }
+                temp[items[i]] = items[i];
+            }
+            return 0;
+        }
+
+        public int FindFirstRecurringNumberUsingList(int[] items)
+        {
+            var temp = new List<int>();
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (temp.Contains(items[i]))
+                {
+                    return items[i];
+                }
+                temp.Add(items[i]);
+            }
+            return 0;
+        }
+
+    }
+}
--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -15,22 +15,27 @@ namespace DataStructuresAndAlgorithms
             //int[] array2 = { 5, 6, 8, 66, 90, 110 };
             //var mergesSortedArrays = a.MergeTwoSortedArrays(array1, array2);
 
-            var hashClass = new HashTableImplementation(2);
-            hashClass.SetValue("Sriditya", 25);
-            hashClass.SetValue("Raj", 26);
-            hashClass.SetValue("David", 55);
-            hashClass.SetValue("Thomas", 21);
-            hashClass.SetValue("Juan", 23);
-            hashClass.SetValue("Kylie", 36);
+            //var hashClass = new HashTableImplementation(2);
+            //hashClass.SetValue("Sriditya", 25);
+            //hashClass.SetValue("Raj", 26);
+            //hashClass.SetValue("David", 55);
+            //hashClass.SetValue("Thomas", 21);
+            //hashClass.SetValue("Juan", 23);
+            //hashClass.SetValue("Kylie", 36);
 
 
 
 
-            var hashTableGetValue = hashClass.GetValue("Raj");
-            var hashTableGetValue2 = hashClass.GetValue("Sriditya");
-            var hashTableGetValue3 = hashClass.GetValue("David");
+            //var hashTableGetValue = hashClass.GetValue("Raj");
+            //var hashTableGetValue2 = hashClass.GetValue("Sriditya");
+            //var hashTableGetValue3 = hashClass.GetValue("David");
 
-            var keys = hashClass.GetKeys();
+            //var keys = hashClass.GetKeys();
+
+            var x = new FindFirstRecurringCharacter();
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4 };
+            var firstRecurringCharacter = x.FindFirstRecurringNumberUsingHash(arr);
+            var firstRecurringCharacterUsingList = x.FindFirstRecurringNumberUsingList(arr);
 
 
         }
